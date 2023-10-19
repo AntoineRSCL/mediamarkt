3ac9bb7e23ccdd3e04b01722632a08a7dfa0cd64
BoubouRSCL <abaut2001@gmail.com>
2023-10-19 10:43:18 +0200

    Connexion Creation de membre modif yaml etc...

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
+
+class AccountController extends AbstractController
+{
+
+    /**
+     * Permet à l'utilisateur de se connecter
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */
+    #[Route('/login', name: 'account_login')]
+    public function index(AuthenticationUtils $utils): Response
+    {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+        $loginError = null;
+        if($error instanceof TooManyLoginAttemptsAuthenticationException)
+        {
+            // l'erreur est due a la limitation de tentavie de connexion
+            $loginError = "Trop de tentatives de connexion. Réesayez plus tard";
+        }
+
+        return $this->render('account/index.html.twig', [
+            'hasError' => $error !== null,
+            'username' => $username,
+            'loginError' => $loginError
+        ]);
+    }
+
+    /**
+     * Permet de se déconnecter
+     *
+     * @return void
+     */
+    #[Route("/logout", name: "account_logout")]
+    public function logout(): void
+    {
+
+    }
+
+    /**
+     * Permet d'afficher le formulaire d'inscription
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @param UserPasswordHasherInterface $hasher
+     * @return Response
+     */
+    #[Route("/register", name:"account_register")]
+    public function register(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //gestion de l'inscription dans la bdd
+
+            $hash = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('account_login');
+        }
+
+        return $this->render("account/registration.html.twig", [
+            "myForm" => $form->createView()
+        ]);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,11 +8,13 @@ use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(fields:['email'], message: "Un autre utilisateur possède deja cette adresse e-mail, merci de la modifier")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -21,6 +23,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Assert\Email(message: "Veuilez renseigner une adresse e-mail valide")]
     private ?string $email = null;
 
     #[ORM\Column]
@@ -30,7 +33,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message: "Veuillez renseigner un mot de passe")]
     private ?string $password = null;
+
+    //Creer a la main pour la confirmation du mot de passe
+    #[Assert\EqualTo(propertyPath:"password", message:"Vous n'avez pas correctement confirme votre mot de passe")]
+    public ?string $passwordConfirm = null;
 
     #[ORM\Column(length: 255)]
     private ?string $firstName = null;
@@ -39,12 +47,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Assert\Url(message: "Veuilelz donner une URL valide pour votre avatar")]
     private ?string $picture = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\Length(min: 10, max: 255, minMessage: "Votre introduction doit faire plus de 10 caractères", maxMessage:"Votre introduction ne doit pas faire plus de 255 caractères")]
     private ?string $introduction = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\Length(min: 100,  minMessage: "Votre description doit faire plus de 100 caractères")]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
